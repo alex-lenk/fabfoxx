f2f0126df923cf0e4ace5e41caae0600558b40cf
artlenk.ru@gmail.com <artlenk.ru@gmail.com>
2016-10-05 13:22:26 +0400

    Created slider-loop

--- src/js/lib.js
+++ src/js/lib.js
@@ -1,4 +1,4 @@
 //= lib/jquery.formstyler.min.js
+//= lib/owl.carousel.min.js
 // = lib/jquery.mCustomScrollbar.js
 // = lib/isotope.pkgd.min.js
-// = lib/owl.carousel.min.js
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    //инициализация плагина от http://dimox.name/jquery-form-styler/
+    // Initializing plugin  http://dimox.name/jquery-form-styler/
     (function ($) {
         $(function () {
             $('.currency-select').styler({
@@ -9,14 +9,30 @@ $(document).ready(function () {
         });
     })(jQuery);
 
-    $('.nav-area-search,.searching-area-close').on('click', function () {
-        $('.searching-area,.searching-area-input').toggleClass("searching-open").focus();
+    //  Main slider - initializing plugin owlCarousel
+    $('.slider-loop').owlCarousel({
+        loop: true,
+        center: true,
+        items: 3,
+        merge: true,
+        margin: 0,
+        autoHeight: true,
+        autoWidth: true,
+        nav: true,
+        navContainerClass: "slider-loop-nav",
+        navText: ['<svg class="left-arrow" viewBox="0 0 46.02 92.06"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#left-arrow"></use></svg>',
+            '<svg class="right-arrow" viewBox="0 0 45.98 91.99"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#right-arrow"></use></svg>']
+    });
+
+    // Different events
+    $('.nav-area-search,.search-area-close').on('click', function () {
+        $('.search-area,.search-area-input').toggleClass("search-area-open").focus();
         $('.nav-area-search').toggleClass("active");
         $('.nav-area-search').removeClass("search-results-show");
 
     });
 
-    $('.searching-area-input').keyup(function () {
+    $('.search-area-input').keyup(function () {
         $('.search-results').addClass('search-results-show');
     });
 });
